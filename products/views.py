0ee92079d6c410008fcfc19d5a56c134c3455909
Martin-ITT <martinusko@gmail.com>
2023-03-19 21:04:47 +0000

    test products added

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+# all products page, inc sort & search
+def all_products(request):
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'products/products.html', context)
