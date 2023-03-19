5a36e3e617cb7dcac98b0c77033d192dc9f5e01b
Martin-ITT <martinusko@gmail.com>
2023-03-19 23:17:31 +0000

    product detail page added#

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -7,9 +7,19 @@ from .models import Product
 def all_products(request):
 
     products = Product.objects.all()
-
     context = {
         'products': products,
     }
     
     return render(request, 'products/products.html', context)
+
+
+# detailed product page
+def product_detail(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    
+    return render(request, 'products/product_detail.html', context)
