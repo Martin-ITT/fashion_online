3348d6ef95e9aba4b9a690ce9dfe681795d077d3
Martin-ITT <martinusko@gmail.com>
2023-03-19 18:50:16 +0000

    admin customized

--- products/models.py
+++ products/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
     name = models.CharField(max_length=50)
     friendly_name = models.CharField(max_length=50, null=True, blank=True)
     is_active = models.BooleanField(null=False, blank=False)
@@ -25,12 +28,13 @@ class Product(models.Model):
     has_size_96 = models.BooleanField(default=False,null=True, blank=True)
     has_size_120 = models.BooleanField(default=False,null=True, blank=True)
     has_size_140 = models.BooleanField(default=False,null=True, blank=True)
+    color = models.CharField(max_length=20, default=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     line_model = models.CharField(max_length=20, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    is_active = models.BooleanField(null=False, blank=False)
+    is_active = models.BooleanField(null=False, blank=False, default=True)
 
     def __str__(self):
         return self.name
