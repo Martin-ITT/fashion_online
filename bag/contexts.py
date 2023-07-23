631e7dc159e685d05813cf653bb187e9bb9bda2a
Martin-ITT <martinusko@gmail.com>
2023-07-23 20:30:48 +0100

    added html shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,6 @@
 from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
@@ -6,6 +8,20 @@ def bag_contents(request):
     total = 0
     product_count = 0
     grand_total = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total = quantity * product.price
+        grand_total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'price': product.price,
+            'quantity': quantity,
+            'product':product,
+            'total': total,
+        })
 
     context = {
         'bag_items': bag_items,
