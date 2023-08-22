fd8a5c6413e179a9ccc2fd3a633000e9891639de
Martin-ITT <martinusko@gmail.com>
2023-08-22 19:40:02 +0000

    S3 bucket implemented - fix 2

--- fashion_online/settings.py
+++ fashion_online/settings.py
@@ -205,6 +205,7 @@ if 'USE_AWS' in os.environ:
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+#
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
