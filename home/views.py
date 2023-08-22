a945b4efff04bfbafc6c93d0a7087da7bd487f8b
Martin-ITT <martinusko@gmail.com>
2023-08-22 16:42:43 +0000

    deploy change home view test removed

--- home/views.py
+++ home/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from products.models import Product # test
 
 # Create your views here.
 
 # home page
 def index(request):
     
-    products = Product.objects.all() # test
-    print(products) # test
-
     return render(request, 'home/index.html')
