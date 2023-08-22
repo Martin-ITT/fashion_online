b88fb1b2d3d1f00d48da80a21144a2efc5aa0344
Martin-ITT <martinusko@gmail.com>
2023-08-22 20:58:01 +0000

    email funcionality added#

--- fashion_online/settings.py
+++ fashion_online/settings.py
@@ -186,6 +186,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # AWS bucket
+
+    # cache control - improves performance
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'fashion-online'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -202,7 +209,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 
 # Default primary key field type
